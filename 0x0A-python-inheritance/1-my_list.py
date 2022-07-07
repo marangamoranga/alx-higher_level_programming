b18de1dbaf8ee43c2aaa9dad06c8c3f2f5b115b1
morangamaranga <hm.maranga@gmail.com>
2022-07-07 11:47:26 -0700

    added mandatory tasks,

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""1-my_list.py"""
+"""Module 1-my_list.
+Creates a class inheriting from the list class.
+"""
 
 
 class MyList(list):
-    """Implementation of a list"""
+    """Class MyList inherits from list."""
 
     def print_sorted(self):
-        """prints the list, but sorted (ascending sort)"""
-        print(sorted(self))
+        """Prints the list, in ascending sort."""
+
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
