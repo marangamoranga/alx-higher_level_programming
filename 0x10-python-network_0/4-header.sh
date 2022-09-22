f7f843aed20a3a1ec20d67a9598a5e8e973df06f
morangamaranga <hm.maranga@gmail.com>
2022-09-22 12:07:11 -0700

    added mandatory tasks

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -H "Hello School!"
+# script that takes URL as arg sends GET request with additional info
+curl -sH "X-Hello School!-User-Id: 98" "$1"
